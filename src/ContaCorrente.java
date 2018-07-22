d0245db9b63f8810f2bbe08b1fda66fe1ca8fcde
Gabriel Castro <gcastrop5@gmail.com>
2018-07-22 02:37:36 -0300

    Final Work

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -5,4 +5,12 @@ public class ContaCorrente extends Conta {
 		super(agencia, numero);
 	}
 
+	// Reescrita de um método da classe Super
+	// Método especifico (Especifico é baseado nas classes filhos)
+	// Sempre que o método é chamado, é tomado como base o método especifico, não o genérico (Classe mãe)
+	@Override
+	public boolean saca(double valor) {
+		double ValorSacar = valor + 0.20;
+		return super.saca(ValorSacar);
+	}
 }
